eb0271855636b9ee06b2ad79e1985abe5a8c1281
Drozdovskiy <158758168+Drozdovskiy@users.noreply.github.com>
2024-09-26 09:16:06 +0300

    Add files via upload

---
+++ CL.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulytor
+{ 
+    internal class CL
+    {
+
+        double a;
+        double b;
+
+        public void minus(double a, double b)
+        {
+            Console.WriteLine(a - b);
+        }
+        public void plus(double a, double b)
+        {
+            Console.WriteLine(a + b);
+        }
+        
+        public void multiplication(double a, double b)
+        {
+            Console.WriteLine(a * b);
+        }
+        public void division(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("На 0 делить нельзя");
+            }
+            else
+            {
+                Console.WriteLine(a / b);
+            }
+
+        }
+
+        public void degree(double a, double b) // степень
+        {
+            Console.WriteLine(Math.Pow(a, b));
+        }
+
+        public void square(double a, double b) // квадрат
+        {
+            Console.WriteLine(Math.Sqrt(a));
+            Console.WriteLine(Math.Sqrt(b));
+        }
+        
+        public void rounding(double a, double b) //округление
+        {
+            Console.WriteLine(Math.Round(a));
+            Console.WriteLine(Math.Round(b));
+        }
+
+        public void arksin(double a, double b) //arksin
+        {
+            Console.WriteLine(Math.Atan(a));
+            Console.WriteLine(Math.Atan(b));
+        }
+
+        public void sin(double a, double b) //sinus
+        {
+            Console.WriteLine(Math.Sin(a));
+            Console.WriteLine(Math.Sin(b));
+        }
+
+        public void cos(double a, double b) //cosinus
+        {
+            Console.WriteLine(Math.Cos(a));
+            Console.WriteLine(Math.Cos(b));
+        }
+
+
+        public void squarePerimeter(double a, double b) //периметр
+        {
+            Console.WriteLine(4 * a);
+            Console.WriteLine(4 * b);
+        }
+
+        public void squreArea(double a, double b) //площадь
+        {
+            Console.WriteLine(Math.Pow(a, 2));
+            Console.WriteLine(Math.Pow(b, 2));
+        }
+
+        public void circleRadius(double a, double b) //радиус круга
+        {
+            Console.WriteLine(a / 2);
+            Console.WriteLine(b / 2);
+        }
+
+        public void circleDiameter(double a, double b) // диаметр круга
+        {
+            Console.WriteLine(2 * a);
+            Console.WriteLine(2 * b);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using Kalkulytor;
+
+start:
+
+
+CL clas = new CL();
+
+string symb;
+
+Console.WriteLine("введите первое число");
+double x = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("введите действие");
+symb = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine("введите второе число");
+double y = Convert.ToDouble(Console.ReadLine());
+
+
+
+switch (symb)
+{
+    case "-":
+        clas.minus(x, y);
+        goto start;
+    case "+":
+        clas.plus(x, y);
+        goto start;
+    case "/":
+        clas.division(x, y);
+        goto start;
+    case "*":
+        clas.multiplication(x, y);
+        goto start;
+
+    case "степень":
+        clas.degree(x, y);
+        goto start;
+    case "квадрат":
+        clas.square(x, y);
+        goto start;
+    case "округление":
+        clas.rounding(x, y);
+        goto start;
+    case "arksin":
+        clas.arksin(x, y);
+        goto start;
+    case "sin":
+        clas.sin(x, y);
+        goto start;
+    case "cos":
+        clas.cos(x, y);
+        goto start;
+
+    case "периметр":
+        clas.squarePerimeter(x, y);
+        goto start;
+    case "площадь":
+        clas.squreArea(x, y);
+        goto start;
+    case "радиус":
+        clas.circleRadius(x, y);
+        goto start;
+    case "диаметр":
+        clas.circleDiameter(x, y);
+        goto start;
+}
